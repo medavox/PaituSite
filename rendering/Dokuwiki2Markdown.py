533a390ba178ab262d0761cb81e402a200042220
Adam Howard <medavox2+github@gmail.com>
2015-09-15 12:08:26 +0100

    merged dokuwiki to markdown conversion files into one. Script is still unfinished; need to finish implementing tables

--- rendering/Dokuwiki2Markdown.py
+++ rendering/Dokuwiki2Markdown.py
@@ -56,42 +56,75 @@ downloadable code blocks "<file <lang> <filename>> <sourcecode> </file>" -> ???
 tag "{{tag><taglist>}} -> "%tag:<taglist>
 """
 
-print "egress"
-
 conversionRules = \
-[("^====== ?([\s\S]+) ?======$","# \1\n\n"),	#headings
-("^===== ?([\s\S]+) ?=====$","## \1\n\n"),
-("^==== ?([\s\S]+) ?====$","### \1\n\n"),
-("^=== ?([\s\S]+) ?===$","#### \1\n\n"),
-("^== ?([\s\S]+) ?==$","##### \1\n\n"),
-("\(\(([\S\s]+)\)\)", "\^\[\1\]"),				#footnotes
-("<sub>([\s\S]+)</sub>","~\1~"),				#subscript
-("<super>([\s\S]+)</super>","~\1~"),			#superscript
-("<del>([\s\S]+)</del>","~~\1~~"),				#strikeout
-("<code( [a-zA-Z]+)?>([\s\S]+)</code>","\n```\1\n\2\n```\n"),#codeblock with optional language attribute
-("''([\S\s]+)''","`\1`"),						#monospace/inline code
-("^  ([\S\s]+)$","\n    \1\n"),					#indented monospace/inline code
-("//([\s\S]+)//","*\1*"),						#italic
-("\[\[(http://[a-zA-Z-_./?&=0-9]+)\|([A-Za-z0-9 ]+)\]\]","[\2](\1)"),						#italic
-#("**([\s\S]+)*","**\1**"),						#bold is the same: **<bold>**
-("{{tag>([a-zA-Z ,]+)}}","%tags:\1")]			#tag list
+[(r"^====== ?([\S \t]+) ?======$", r"# \1"),					#headings
+(r"^===== ?([\S \t]+) ?=====$", r"## \1"),
+(r"^==== ?([\S \t]+) ?====$", r"### \1"),
+(r"^=== ?([\S \t]+) ?===$", r"#### \1"),
+(r"^== ?([\S \t]+) ?==$", r"##### \1"),
+(r"\(\(([\S \t]+)\)\)", r"\^\[\1\]"),							#footnotes
+(r"<sub>([\S \t]+)</sub>", r"~\1~"),							#subscript
+(r"<super>([\S \t]+)</super>", r"~\1~"),						#superscript
+(r"<del>([\S \t]+)</del>", r"~~\1~~"),							#strikeout
+(r"<code (.+)>([\S \n\t]+)</code>", r"```\1\n\2\n```\n"),		#codeblock with language attribute
+(r"<code>(.+)</code>", r"\n```\n\1\n```\n"),					#codeblock without language attribute
+(r"''([\S \t]+)''", r"`\1`"),									#monospace/inline code
+(r"^  ([^-*][\S \t]+)$", r"    \1"),							#indented monospace/inline code
+#(r"//([\S \t]+)//", r"*\1*"),									#italic; match pattern currently broken
+(r"\[\[(https?://[a-zA-Z-_./?&=0-9+,#]+)\|([A-Za-z0-9 ]+)\]\]", r"[\2](\1)"),#links with labeltext
+(r"\[\[(https?://[a-zA-Z-_./?&=0-9+,#]+)\]\]", r"<\1>"),		#links without labeltext
+(r"^([ \t]*)-( ?[\w])", r"\1#. \2"),							#numbered lists
+#(r"^(  \*([\w])", r"\* \1"),									#tidying unordered lists
+(r"^\^ ?([\S \t]+\^)+$", r""),										#tables:headers
+#(r"^(\^ ?([\S \t])+)+", r"\2"),								#tables:headers
+#(r"^\^ ?", r"| "),												#tables:remove starting ^ in headers
+#(r"(([\S \t])+)+\^", r"\1|"),									#tables:convert header from ^ to |
+#(r"([\S \t])+\^.*$", r"\=|---"),								#tables:a from ^ to |
+#tables:generate header pipe row
+(r"{{tag>([a-zA-Z ,]+)}}", r"\n%tags:\1\n")]					#tag list
+#(r"^{{tag>(.*) (.*)}}", r"\n%tags:\1,\2\n")]						#tag list
+#unordered list
+#tables...
 
+#("**([\S \t]+)*","**\1**"),									#bold is the same: **<bold>**
+#(r"<code(.?( [a-zA-Z]+)?)>([\S \t]+)</code>", r"\n```\1\n\2\n```\n"),#codeblock with optional language attribute
+#(r"<code(.?( [a-zA-Z]+)?)>([\S \t]+)</code>", r"\n```\1\n\2\n```\n"),#codeblock with optional language attribute
+#conversionRules = [(r"^====== ?([A-Za-z 0-9:-]+) ?======$", r"# \1\n\n")]
 
-print "Lol"
+#print "Lol"
+
+def tableConvert(matchObject):
+	print string
+	tokens = matchObject.string.split('^')
+	output = ""
+	for token in tokens:
+		output = output + token + ("|" if tokens[-1] == token else "")
+	return output
+	#string.replace("{{
+	#pat = re.compile("", re.M)
+
+def tagConvert(matchObject):
+	
 
 for cwd, dirs, files in os.walk('../pages'):
 	for f in files:
 		if f[-3:] == 'txt':
 			print("converting "+f)
-			#fyl = open(os.path.join(cwd,f), 'r')
-			fyl = open("../pages/almondmilk.txt", 'r')
+			fyl = open(os.path.join(cwd,f), 'r')
+			#fyl = open("../pages/almondmilk.txt", 'r')
 			pageString = fyl.read()
 			fyl.close()
 			for rule in conversionRules:
 				print rule
-				pat = re.compile(rule[0])
-				pageString = pat.sub(rule[1], pageString)
-				print pageString
+				pat = re.compile(rule[0], re.M)
+				#mo = pat.search(pageString)
+				#print mo
+				#print mo.groups()
+				pageString = re.sub(pat, rule[1], pageString)
+			pati  = re.compile(r"^\^ ?([\S \t]+\^)+$", re.M) #table head match rule
+			patty = re.compile(r"^\^ ?([\S \t]+\^)+$", re.M) #table head match rule
+			pageString = re.sub(pat, tableConvert, pageString)
+			
 			converted = open(os.path.join("../converted", f[:-3]+"md"), 'w')
 			converted.write(pageString)
 			converted.close()
