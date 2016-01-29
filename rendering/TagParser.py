6664b85c27ec5daa0654984ab3a678a191c88229
adam <medavox2+github@gmail.com>
2016-01-29 02:31:53 +0000

    continued replacing nearly all other rendering files with render.py

--- rendering/TagParser.py
+++ rendering/TagParser.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python
 import os, sys, re
-from getPageTitle import getTitle
-#from finalPreprocessor import getTags
+#from render import getTitle
 
 """
 Creates a page for each tag, with links to each article with that tag.
 Tags are found by checking all articles
 """
-
-#if len(sys.argv) < 2:
-#	print "usage:" + sys.argv[0] +  """<dir>
-#	where <dir> is a root dir of files you wish to analyse recursively."""
-#	sys.exit(0)
-
 tagDict = dict()
 
 def getTags(filename):
 	openfile = open(filename, 'r')
-	for line in openfile.readlines():
-		if line.startswith("%tag") and ':' in line:#make sure the "%tag" line contains a ":"
-			tagStartIndex = line.index(':')+1
-			if line[tagStartIndex] == ' ':
-				tagStartIndex = tagStartIndex +1
-			tagString = line[tagStartIndex:]#remove the leading "%tag:" from the start of line
-			pat = re.compile(' ?, ?') #remove any spaces before or after the separating comma; but keep tag-internal spaces
-			cleanedCommas = pat.sub( ',', tagString)
-			if cleanedCommas[-1] == '\n':
-				cleanedCommas = cleanedCommas[:-1] #strip trailing newline
-			#print tagString
-			#tagList = 
-			openfile.close()
-			return cleanedCommas.split(',')
+	contents = openfile.read()
+	pat = re.compile(r"^%tags?: ?([^,]+(,[^,]+)*)$")
+	retlist = []
+	for mo in pat.finditer(contents, re.M):
+		rawTags = mo.group(1)
+		pat = re.compile(' ?, ?') #remove any spaces before or after the separating comma; but keep tag-internal spaces
+		cleanedCommas = pat.sub( ',', rawTags)
+		retlist = retlist + cleanedCommas.split(',')
+	return retlist
 
-for cwd, dirs, files in os.walk('../articles'):
-	for f in files:
+#generate navbar links to tag pages, as an add-in snippet
+#generate the markdown for tag pages
+def parseTags(nope):
+	#parse %tags, creating a global one-to-many associative array (dictionary) of tags to pages
+	for f in os.listdir("../articles"):
 		if f[-3:] == '.md':
-			#parse %tags, creating a global one-to-many associative array (dictionary) of tags to pages
-			tagList = getTags(os.path.join(cwd,f))
+			tagList = getTags("../articles/"+f)
 			#print f+":"+str(type(tagList))
 			if type(tagList) is list:
 				for tag in tagList:
@@ -45,26 +35,25 @@ for cwd, dirs, files in os.walk('../articles'):
 						tagDict[tag].append(f)
 					else: #initialise this tagkey with a new list containing this filename
 						tagDict[tag] = [f]
-
-#generate navbar links to tag pages, as an add-in snippet
-#generate the markdown for tag pages
-tagEntries = open('../temp/tagEntries.html', 'w')
-tagEntries.write("All Tags\n===\n")
-for tag in tagDict.keys():
-	#print tag
-	#consider sorting tags before printing
-	linkUrl = ""+tag
-	tagEntries.write("* "+tag+"\n")
-	tagEntries.write('\n\t\t\t\t\t<li class=\"pure-menu-item\">\n\t\t\t\t\t\t<a href=\"'+linkUrl+
-		'" class="pure-menu-link">'+tag+'</a>\n\t\t\t\t\t</li>')
-	tagPage = open('../temp/'+tag.replace(' ', '_').lower()+'.md', 'w')
-	#tagPage.write("Pages Tagged '"+tag+"'\n===\n\n") # write page title
-	for page in tagDict[tag]:
-		link = page.lower().replace(' ', '_')[:-3]+".html"
-		title = getTitle("../articles/"+page)
-		tagPage.write("* ["+title+"]("+link+")\n")
-		print tag+":"
-		print "\t\""+title+"\" at "+link
-	tagPage.close()
 	
-tagEntries.close()
+	#tagEntries = open('../temp/all_tags.html', 'w')
+	#tagEntries.write("# All Tags\n")
+	
+	#create a page for every found tag.
+	#on that page, add a link to every article with that tag
+	for tag in tagDict.keys():
+		#consider sorting tags before printing
+	#	linkUrl = ""+tag
+	#	tagEntries.write("* "+tag+"\n")
+	#	tagEntries.write('\n\t\t\t\t\t<li class=\"pure-menu-item\">\n\t\t\t\t\t\t<a href=\"'+linkUrl+
+	#		'" class="pure-menu-link">'+tag+'</a>\n\t\t\t\t\t</li>')
+		tagPage = open('../temp/'+tag.replace(' ', '_').lower()+'.md', 'w')
+		#tagPage.write("Pages Tagged '"+tag+"'\n===\n\n") # write page title
+		for page in tagDict[tag]:
+			link = page.lower().replace(' ', '_')[:-3]+".html"
+			title = getTitle("../articles/"+page)
+			tagPage.write("* ["+title+"]("+link+")\n")
+			print tag+":"
+			print "\t\""+title+"\" at "+link
+		tagPage.close()
+	tagEntries.close()
--- rendering/render.py
+++ rendering/render.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import re,sys,os,os.path,subprocess
+from tweaks import preProcess
+from TagParser import parseTags
 #shopt -s extglob ##enable better bash regex support, ie for the ?(pattern) below
 
 fileChanged = False
@@ -8,6 +10,24 @@ titles = dict()
 def guaranteeFolder(folderName):
 	if not os.path.isdir(folderName):
 		os.mkdir(folderName, 0744)
+
+"""
+Creates a page for each tag, with links to each article with that tag.
+Tags are found by checking all articles
+"""
+tagDict = dict()
+
+def getTags(filename):
+	openfile = open(filename, 'r')
+	contents = openfile.read()
+	pat = re.compile(r"^%tags?: ?([^,]+(,[^,]+)*)$")
+	retlist = []
+	for mo in pat.finditer(contents, re.M):
+		rawTags = mo.group(1)
+		pat = re.compile(' ?, ?') #remove any spaces before or after the separating comma; but keep tag-internal spaces
+		cleanedCommas = pat.sub( ',', rawTags)
+		retlist = retlist + cleanedCommas.split(',')
+	return retlist
 
 """
 returns the derived title of a given mdfile, whether from its markdown title, or its file name.
@@ -32,6 +52,44 @@ def getTitle(filename):
 			return (nopath[:-3], False)
 		else:
 			return (nopath, False)
+
+#generate navbar links to tag pages, as an add-in snippet
+#generate the markdown for tag pages
+def parseTags(nope):
+	#parse %tags, creating a global one-to-many associative array (dictionary) of tags to pages
+	for f in os.listdir("../articles"):
+		if f[-3:] == '.md':
+			tagList = getTags("../articles/"+f)
+			#print f+":"+str(type(tagList))
+			if type(tagList) is list:
+				for tag in tagList:
+					if tag in tagDict:
+						#add this file's name to listvalue of this tagkey
+						tagDict[tag].append(f)
+					else: #initialise this tagkey with a new list containing this filename
+						tagDict[tag] = [f]
+	
+	#tagEntries = open('../temp/all_tags.html', 'w')
+	#tagEntries.write("# All Tags\n")
+	
+	#create a page for every found tag.
+	#on that page, add a link to every article with that tag
+	for tag in tagDict.keys():
+		#consider sorting tags before printing
+	#	linkUrl = ""+tag
+	#	tagEntries.write("* "+tag+"\n")
+	#	tagEntries.write('\n\t\t\t\t\t<li class=\"pure-menu-item\">\n\t\t\t\t\t\t<a href=\"'+linkUrl+
+	#		'" class="pure-menu-link">'+tag+'</a>\n\t\t\t\t\t</li>')
+		tagPage = open('../temp/'+tag.replace(' ', '_').lower()+'.md', 'w')
+		#tagPage.write("Pages Tagged '"+tag+"'\n===\n\n") # write page title
+		for page in tagDict[tag]:
+			link = page.lower().replace(' ', '_')[:-3]+".html"
+			title = getTitle("../articles/"+page)
+			tagPage.write("* ["+title+"]("+link+")\n")
+			print tag+":"
+			print "\t\""+title+"\" at "+link
+		tagPage.close()
+	#tagEntries.close()
 
 for liveFile in os.listdir("../articles"):
 	if liveFile.endswith(".md"):
@@ -60,7 +118,7 @@ for liveFile in os.listdir("../articles"):
 
 #todo: need to regenerate tags into bash dictionary once tagpage mdfiles have been generated
 if fileChanged: #if any files have changed, regenerate the tags
-	TagParser	#regenerate tag pages
+	parseTags(None)	#regenerate tag pages
 
 guaranteeFolder("../html")
 guaranteeFolder("../lastinput")
